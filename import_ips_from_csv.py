a9f70b468d37a5e6034c0e6c900628d11315c264
Marek Kowalski <kowalski0123@gmail.com>
2019-03-26 15:05:44 +0100

    Initial commit

---
+++ import_ips_from_csv.py
@@ -0,0 +1,46 @@
+import argparse
+from functools import reduce
+
+import boto3
+
+TABLE_NAME = 'restrict-ip-table'
+
+
+def format_ip(ip):
+    def shift_and_sum(x, y):
+        print(x, y)
+        return (int(x) << 8) + int(y)
+    return reduce(shift_and_sum, ip.split('.'))
+
+
+def import_csv_to_dynamodb(table_name, csv_file_name):
+    '''
+    Import a CSV file to a DynamoDB table
+    '''
+    session = boto3.session.Session()
+    dynamodb = session.resource('dynamodb')
+    dynamodb_table = dynamodb.Table(table_name)
+    csv_file = open(csv_file_name, 'r')
+
+    with dynamodb_table.batch_writer() as batch:
+        for no, cur_line in enumerate(csv_file):
+            row = {'ip': format_ip(cur_line)}
+            print('{} {}'.format(no, row))
+            batch.put_item(Item=row)
+
+    csv_file.close()
+
+
+parser = argparse.ArgumentParser(description='Upload list of IPs to dynamo.')
+parser.add_argument('csv_file_name', help='filename with ips to upload')
+parser.add_argument(
+    '--table-name', help='dynamodb table name', default=TABLE_NAME)
+
+
+def main():
+    args = parser.parse_args()
+    import_csv_to_dynamodb(args.table_name, args.csv_file_name)
+
+
+if __name__ == "__main__":
+    main()
---
+++ restrictIp.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const QS = require('querystring')
+const AWS = require('aws-sdk')
+const ssm = new AWS.SSM()
+const dynamodb = new AWS.DynamoDB()
+
+exports.handler = (event, context, callback) => {
+  const request = event.Records[0].cf.request
+  const uri = request.uri
+  checkIpTable(request, callback)
+}
+
+const TableName = 'restrict-ip-table'
+
+async function checkIpTable(request, callback) {
+  const ipnumber = ip2int(request.clientIp).toString()
+  let data
+  try {
+    data = await dynamodb.getItem({
+      Key: { ip: { N: ipnumber } },
+      TableName
+    }).promise()
+  } catch (err) {
+    console.error(err)
+    restrictedResponse(request, callback)
+    return
+  }
+  if (!data.hasOwnProperty('Item')) {
+    callback(null, request)
+    return
+  } else {
+    await checkRestrictFlag(request, callback)
+  }
+}
+
+async function checkRestrictFlag(request, callback) {
+  const params = QS.parse(request.querystring)
+  if (!params.ipr) {
+    return restrictedResponse(request, callback)
+  }
+  const ssmParams = { Name: '/develop/shared/restrict_flag', WithDecryption: true }
+  let data
+  try {
+    data = await ssm.getParameter(ssmParams).promise()
+  } catch (err) {
+    console.error(err)
+    restrictedResponse(request, callback)
+  }
+  if (data.Parameter.Value !== params.ipr) {
+    restrictedResponse(request, callback)
+  } else {
+    callback(null, request)
+  }
+}
+
+function ip2int(ip) {
+  return ip.split('.').reduce((ipInt, octet) => (ipInt << 8) + parseInt(octet, 10), 0)
+}
+
+function restrictedResponse(request, callback) {
+  request.uri = '/restricted.html'
+  callback(null, request)
+}
